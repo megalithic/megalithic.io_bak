91f271b2d77f744c8a291f4b562e140770cde8da
Seth Messer <seth.messer@gmail.com>
2019-01-22 12:23:34 -0600

    Fixes paths and Timex usage instead of Calendar

--- lib/megalithic_web/controllers/page_controller.ex
+++ lib/megalithic_web/controllers/page_controller.ex
@@ -2,7 +2,7 @@ defmodule MegalithicWeb.PageController do
   use MegalithicWeb, :controller
 
   def index(conn, _params) do
-    {:ok, posts} = Megalithic.BlogPostRetriever.list()
+    {:ok, posts} = Megalithic.BlogService.list()
     render(conn, "index.html", posts: posts)
   end
 end
