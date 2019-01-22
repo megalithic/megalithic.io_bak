b9272019d345cc8455c20c47107736c7c07a3e78
Seth Messer <seth.messer@gmail.com>
2019-01-21 21:41:01 -0600

    bump versions

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule Megalithic.MixProject do
   def project do
     [
       app: :megalithic,
-      version: "0.1.0",
-      elixir: "~> 1.5",
+      version: "0.1.1",
+      elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
