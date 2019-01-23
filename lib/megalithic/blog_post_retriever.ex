0b8c86540f19bf4aca4ebf8fdbc45c60df83ca0f
Seth Messer <seth.messer@gmail.com>
2019-01-23 15:24:48 -0600

    switches to use a helper to get the priv dir

--- lib/megalithic/blog_post_retriever.ex
+++ lib/megalithic/blog_post_retriever.ex
@@ -1,14 +1,20 @@
 defmodule Megalithic.BlogPostRetriever do
   def retrieve do
-    case File.ls("priv/posts") do
-      {:ok, files} ->
-        files
-        |> Enum.map(&Megalithic.BlogPost.compile/1)
-        |> Enum.sort(&sort/2)
+    :code.priv_dir(:megalithic)
+    |> Path.join("posts")
+    |> File.ls!()
+    |> Enum.map(&Megalithic.BlogPost.compile/1)
+    |> Enum.sort(&sort/2)
 
-      {:error, _reason} ->
-        File.mkdir_p!("priv/posts")
-    end
+    # case File.ls("priv/posts") do
+    #   {:ok, files} ->
+    #     files
+    #     |> Enum.map(&Megalithic.BlogPost.compile/1)
+    #     |> Enum.sort(&sort/2)
+
+    #   {:error, _reason} ->
+    #     File.mkdir_p!("priv/posts")
+    # end
   end
 
   def sort(a, b) do
